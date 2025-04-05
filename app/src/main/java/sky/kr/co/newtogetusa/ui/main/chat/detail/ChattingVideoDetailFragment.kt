e1e0438bf00c313bea28305bd46ad9699defdb5a
dcmoon <joynlook@gmail.com>
2025-04-05 23:46:41 +0900

    채팅UI(비디오 타입) 작업중.

--- app/src/main/java/sky/kr/co/newtogetusa/ui/main/chat/detail/ChattingVideoDetailFragment.kt
+++ app/src/main/java/sky/kr/co/newtogetusa/ui/main/chat/detail/ChattingVideoDetailFragment.kt
@@ -1,13 +1,20 @@
 package sky.kr.co.newtogetusa.ui.main.chat.detail
 
+import android.annotation.SuppressLint
+import android.os.Handler
+import android.widget.SeekBar
 import androidx.fragment.app.viewModels
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import sky.kr.co.newtogetusa.R
 import sky.kr.co.newtogetusa.databinding.FragmentChattingVideoDetailBinding
 import sky.kr.co.newtogetusa.ui.base.BaseFragment
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class ChattingVideoDetailFragment : BaseFragment<FragmentChattingVideoDetailBinding, ChattingVideoDetailViewModel>() {
@@ -16,16 +23,88 @@ class ChattingVideoDetailFragment : BaseFragment<FragmentChattingVideoDetailBind
     override val viewModel: ChattingVideoDetailViewModel by viewModels()
     val args : ChattingVideoDetailFragmentArgs by navArgs()
 
+    private val handler = Handler()
     private lateinit var exoPlayer: ExoPlayer
+    private var totalPlayTime:String = "00:00"
 
     override fun init() {
         super.init()
-
+        dataBinding.viewModel = viewModel
+        dataBinding.lifecycleOwner = this
         exoPlayer = ExoPlayer.Builder(requireContext()).build()
         dataBinding.videoPlayer.player = exoPlayer
 
         exoPlayer.setMediaItem(MediaItem.fromUri(args.videoUrl))
         exoPlayer.prepare()
+        exoPlayer.addListener(object : Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                if (playbackState == Player.STATE_READY) {
+                    val totalDuration = exoPlayer.duration
+                    dataBinding.seekbar.max = totalDuration.toInt()
+                    totalPlayTime = formatTime(totalDuration)
+                }
+            }
+        })
         exoPlayer.play()
+        updateSeekBar()
+
+        dataBinding.seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            @SuppressLint("SetTextI18n")
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    exoPlayer.seekTo(progress.toLong())
+                    dataBinding.tvTime.text = formatTime(progress.toLong())+"/"+totalPlayTime
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
+
+    override fun initObserver() {
+        super.initObserver()
+
+        viewModel.event.observe(viewLifecycleOwner){ event ->
+            when(event){
+                ChattingVideoDetailViewModel.Event.Back ->{
+                    findNavController().popBackStack()
+                }
+                ChattingVideoDetailViewModel.Event.PausePlay ->{
+                    exoPlayer.let {
+                        if (it.isPlaying) {
+                            it.pause()
+                        } else {
+                            it.play()
+                            updateSeekBar()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        exoPlayer.release()
+    }
+
+    private fun updateSeekBar() {
+        exoPlayer.let {
+            dataBinding.seekbar.progress = it.currentPosition.toInt()
+            dataBinding.tvTime.text = formatTime(it.currentPosition)+"/"+totalPlayTime
+            handler.postDelayed({ updateSeekBar() }, 500)
+        }
+    }
+
+    // 시간을 00:00 형식으로 변환
+    private fun formatTime(ms: Long): String {
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(ms)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(ms) % 60
+        return String.format("%02d:%02d", minutes, seconds)
     }
 }
--- app/src/main/java/sky/kr/co/newtogetusa/ui/main/chat/detail/ChattingVideoDetailViewModel.kt
+++ app/src/main/java/sky/kr/co/newtogetusa/ui/main/chat/detail/ChattingVideoDetailViewModel.kt
@@ -1,10 +1,22 @@
 package sky.kr.co.newtogetusa.ui.main.chat.detail
 
+import androidx.lifecycle.LiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import sky.kr.co.newtogetusa.base.SingleLiveEvent
 import sky.kr.co.newtogetusa.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ChattingVideoDetailViewModel @Inject constructor() : BaseViewModel() {
 
+    private val _event = SingleLiveEvent<Event>()
+    val event: LiveData<Event> = _event
+    fun onEventClick(event: Event){
+        _event.value = event
+    }
+
+    sealed class Event{
+        object Back : Event()
+        object PausePlay : Event()
+    }
 }
